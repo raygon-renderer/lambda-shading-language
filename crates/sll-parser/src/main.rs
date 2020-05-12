e55f6e6cebae5e820c8cb7c57e383343bdd7c5cb
novacrazy <novacrazy@gmail.com>
2020-05-11 19:19:24 -0500

    Continue WIP grammar

--- crates/sll-parser/src/main.rs
+++ crates/sll-parser/src/main.rs
@@ -9,26 +9,61 @@ use pest::Parser;
 pub struct GrammarParser;
 
 fn main() {
-    let test =
-r#"
+    let test = r##"
 struct Test {
     a: i32,
     b: [f32; 4],
-    c: Vector3
+    c: *mut Vector3
 }
 
-fn other() {
+const HELLO: str = r#"
+Hello, World!
+Testing
+"#;
 
+fn other(_x: f32, Test { a: y, b: _, c }: Test) {
+    // do something
+}
+
+fn main() {
+    let v: Vector3 = Vector3(1, 2, 3);
+
+    let x: Test = Test {
+        a: (0 as i32) as u32,
+        b: [1, 2, 3, 4],
+        c: &v
+    };
+
+    let b: i32 = x.a;
 }
 
 fn add(x: i32, y: i32) -> i32 {
     let z: i32 = x + y;
 
-    other();
+    let h: i32 = if z < 2 {
+        y = 0;
+        z + 2
+    } else if y < 0 {
+        z += 1;
+
+        0
+    } else {
+        1
+    };
+
+    for i in 0..4 {
+        z += i;
+    }
+
+    while z < 10 {
+        z += 1;
+    }
+
+    other(z + 5);
 
     z + 1
 }
-"#;
+"##;
 
     let parsed = GrammarParser::parse(Rule::main, test);
 
